54357b21e36e34ae38013feddcf0a5ccbad44140
Mosh <moshjahan@users.noreply.github.com>
2021-10-16 13:05:14 +0100

    Update Client Script.js

--- ServiceNow/Widget/Donut Graph/Client Script.js
+++ ServiceNow/Widget/Donut Graph/Client Script.js
@@ -1,59 +1,53 @@
-(function(){
+function()
+{
+	var c = this;
 	
-  // Get maximum count
-	
-  var maximumItems = new GlideAggregate(options.data_table);
+	var initializeWidget = function(demoMode)
+	{
+		console.log(c.data);
 
-  maximumItems.addQuery(options.maximum_count_query);
-  maximumItems.addAggregate('COUNT');
-  maximumItems.query();
+		if (demoMode)
+		{
+			setInterval(c.demoMode, 3000);
+		}
+		else
+		{
+			setTimeout(function(){ c.updateGraph(c.data.graph.value); }, 1000);
+		}
+	};
+	
+	this.demoMode = function()
+	{
+		var r = Math.floor(Math.random() * 100);
+		c.updateGraph(r);
+	};
+	
+	this.updateGraph = function(value)
+	{
+		var container = document.getElementById(c.data.graph.instanceId);
+		container.style.setProperty("--DonutMetricValue", value);		
+	};
+	
+	this.graphClick = function()
+	{
+		console.log("TEST");
+		
+		var input =
+		{
+			action : "TEST"
+		};
+		
+		c.server.get(input).then(function(response)
+		{
+			c.data = response.data;
+			
+			if (c.data.graph)
+			{
+				console.log(c.data.graph);
+				c.updateGraph(c.data.graph.value);
+			}
+		});
+	};
 
-  var maximumCount = 100;
-
-  if (maximumItems.next())
-  {
-    maximumCount = maximumItems.getAggregate("COUNT");
-  }
-	
-  // Get current count
-	
-  var currentItems = new GlideAggregate(options.data_table);
-
-  currentItems.addQuery(options.current_count_query);
-  currentItems.addAggregate('COUNT');
-  currentItems.query();
-
-  var currentCount = 0;
-
-  if (currentItems.next())
-  {
-    currentCount = currentItems.getAggregate("COUNT");
-  }
-
-  // Calculate percentage value
-	
-  var percentage = Math.round(currentCount / maximumCount * 100);
-	
-  data.graph = {};
-  data.graph.value = percentage;
-  data.graph.instanceId = options.unique_instance_id;
-  data.graph.initialValue = options.initial_value;
-  data.graph.captionText = options.caption_text;	
-  data.graph.infoText = options.info_text;
-  data.graph.style = {};
-  data.graph.style.outerShadow = options.outer_shadow;
-  data.graph.style.innerShadow = options.inner_shadow;
-  data.graph.style.ringColor = options.ring_color;
-  data.graph.style.infoTextColor = options.info_text_color;
-	
-  data.debug = { options : options };
-	
-  if (input)
-  {
-    if (input.action === "TEST")
-    {
-      data.graph.value = Math.floor(Math.random() * 100);
-    }
-  }
-	
-})();
+	initializeWidget();
+}
